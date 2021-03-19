d21b82fc5f611612989444f25696db879c60ed23
Cynthia Teeters <cynthiateeters@users.noreply.github.com>
2021-03-19 18:32:02 -0400

    Delete main.js

--- main.js
+++
@@ -1,6 +0,0 @@
-/* ATTENTION: THIS IS CODE FROM THE YOUTUBE TUTORIAL. IT IS NOT MEANT TO RUN, IT IS JUST FOR LEARNING PURPOSES 
-
-Video by Kyle Cook
-How To Build A Simple Card Game With JavaScript
-https://youtu.be/NxRwIZWjLtE
- */
