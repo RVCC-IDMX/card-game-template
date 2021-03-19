b509b6c63c01006cc2c1c4e4bbcb077d8b74585e
Cynthia Teeters <cynthiateeters@users.noreply.github.com>
2021-03-19 18:30:22 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,6 @@
-/* ATTENTION: THIS IS CODE FROM THE YOUTUBE CRASH COURSE. IT IS NOT MEANT TO RUN, IT IS JUST FOR LEARNING PURPOSES 
+/* ATTENTION: THIS IS CODE FROM THE YOUTUBE TUTORIAL. IT IS NOT MEANT TO RUN, IT IS JUST FOR LEARNING PURPOSES 
 
-Video by Brad Traversy
-JavaScript Crash Course For Beginners
-https://youtu.be/hdI2bqOjy3c?t=4228 
-
-Start at 1:10:29
+Video by Kyle Cook
+How To Build A Simple Card Game With JavaScript
+https://youtu.be/NxRwIZWjLtE
  */
